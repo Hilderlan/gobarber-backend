2d7322f2fda6cf0f6f2e4845cd1e0107364e5bbc
Hilderlan <hilakt9698@gmail.com>
2020-06-29 08:38:09 -0300

    Config ESLint and Prettier finished

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({message: 'Hello World!'});
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World!" });
 });
 
 app.listen(5000, () => {
-  console.log('Server started!');
-})
+  console.log("Server started!");
+});
