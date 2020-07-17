640da201e2ee6f4be93913a0508a5d0cdb38ab60
Hilderlan <hilakt9698@gmail.com>
2020-07-17 09:21:49 -0300

    Tests done for the avatar functionality.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,12 +2,13 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
-const pathName = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: pathName,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
   storage: multer.diskStorage({
-    destination: pathName,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
