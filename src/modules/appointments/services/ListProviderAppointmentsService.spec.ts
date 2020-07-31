5fb8437653ceb13d72d6fdeb98186d208f7dddfb
Hilderlan <hilakt9698@gmail.com>
2020-07-31 11:02:20 -0300

    Functionality of listing a provider's appointments made

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+// import AppError from '@shared/errors/AppError';
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentRepository: FakeAppointmentRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a especific day', async () => {
+    const appointment1 = await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 14),
+    });
+
+    const appointment2 = await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 16),
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      day: 20,
+      month: 7,
+      year: 2020,
+      provider_id: 'provider',
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
