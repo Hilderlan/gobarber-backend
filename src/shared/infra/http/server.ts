4edc31f38bf97096f292a8c1074bebc01528820a
Hilderlan <hilakt9698@gmail.com>
2020-08-01 01:03:02 -0300

    Validation of input data with Celebrate and Joi

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'reflect-metadata';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -16,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
+
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
